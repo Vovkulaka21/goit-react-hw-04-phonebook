c1f80643d65e5d6e83ad66e0a312fc626c44abc4
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2024-02-27 19:39:04 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import css from './App.module.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import AddContactForm from './AddContactForm/AddContactForm';
@@ -8,11 +8,15 @@ import Phonebook from './Phonebook/Phonebook';
 import ContactFilter from './ContactFilter/ContactFilter';
 
 const App = () => {
-  const [contacts, setContacts] = useState([{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}]);
+  const [contacts, setContacts] = useState(() => {
+    const data = JSON.parse(localStorage.getItem('saved-contacts'));
+    return data || [];
+  });
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const dublicateName = ({ name }) => {
     const normalizeName = name.toLowerCase().replaceAll(' ', '');
@@ -47,7 +51,6 @@ const App = () => {
   const inputFilter = ({ target }) => setFilter(target.value);
 
   const getFilteredContacts = () => {
-
     if (!filter) {
       return contacts;
     }
@@ -72,39 +75,10 @@ const App = () => {
       <h2>Contacts</h2>
       <div className={css.contacts_box}>
         <ContactFilter inputFilter={inputFilter} />
-        <Phonebook
-          items={items}
-          deleteContact={deleteContact}
-        />
+        <Phonebook items={items} deleteContact={deleteContact} />
       </div>
     </div>
   );
 };
-
-/* class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    
-    const contacts = JSON.parse(localStorage.getItem('saved-contacts'));
-    if (contacts?.length) {
-      this.setState({
-        contacts,
-      });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem(
-        'saved-contacts',
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
-} */
 
 export default App;
